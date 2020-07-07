6b908447b10a0c672aab48c7bf2bf62a3b32d75d
lokesh53941 <65780262+lokesh53941@users.noreply.github.com>
2020-07-07 16:18:03 +0530

    initial commit

---
+++ numpyexercise.py
@@ -0,0 +1,7 @@
+import numpy
+import cv2
+n=numpy.arange(27)
+a=n.reshape(3,3,3)
+print(a)
+print(cv2.imread("C:\Lokesh\Repositories\numpy\numpyUdemyExercise\smallgray.png",0))
+#cv2.imwrite("C:\Lokesh\Repositories\numpy\numpyUdemyExercise\abc.png",df1)
